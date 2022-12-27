4a283b86f2eb9cd4aae5ce37a7500d516496ba01
BossWasHere <22352723+BossWasHere@users.noreply.github.com>
2022-12-27 16:23:17 +0000

    fix: token header format

--- src/middlewares/jwt.m.ts
+++ src/middlewares/jwt.m.ts
@@ -10,7 +10,11 @@ export default function (req: Request, res: Response, next: NextFunction) {
             .json({ error: 'No authorization token provided' });
     }
 
-    const decoded = verifyJWT(token);
+    if (!token.startsWith('Bearer ')) {
+        return res.status(401).json({ error: 'Invalid authorization token' });
+    }
+
+    const decoded = verifyJWT(token.substring(7));
 
     if (decoded == null) {
         return res.status(401).json({ error: 'JWT is not valid' });
